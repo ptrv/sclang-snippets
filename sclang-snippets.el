e1589165c407d05f9916e756e3e0e83a56d7e8d7
ptrv <mail@petervasil.net>
2013-05-13 01:43:28 +0200

    Turn into proper package

---
+++ sclang-snippets.el
@@ -0,0 +1,47 @@
+;;; sclang-snippets.el --- Snippets for sclang-mode
+
+;; Copyright (C) 2013 ptrv <mail@petervasil.net>
+
+;; Author:  ptrv <mail@petervasil.net>
+;; Keywords: snippets
+;; Version 1.0.0
+;; Package-Requires: ((yasnippet "0.8.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Snippets for SuperCollider Emacs mode.
+
+;;; Code:
+
+(eval-when-compile
+  (require 'yasnippet))
+
+(defvar sclang-snippets-dir (file-name-directory (or (buffer-file-name)
+                                                   load-file-name)))
+
+;;;###autoload
+(defun sclang-snippets-initialize ()
+  (let ((snip-dir (expand-file-name "snippets" sclang-snippets-dir)))
+    (add-to-list 'yas-snippet-dirs snip-dir t)
+    (yas-load-directory snip-dir)))
+
+;;;###autoload
+(eval-after-load "yasnippet"
+  '(sclang-snippets-initialize))
+
+(provide 'sclang-snippets)
+
+;;; sclang-snippets.el ends here
